b6a0a2dae863fb5907e5319938984f009092c74f
Buthmika <buthmikamenushan@gmail.com>
2025-04-14 11:39:54 +0530

    Update db.js

--- personManagmentConsoleApp/db.js
+++ personManagmentConsoleApp/db.js
@@ -1,4 +1,13 @@
-console.log("db.js working")
+// console.log("db.js working")
 let name="buthmika";
 let age=23;
-module.exports=name;
+const add=()=>{
+      console.log("add function")
+}
+const view=()=>{
+      console.log("view function")
+}
+module.exports={
+      add:add,
+      view:view
+}
