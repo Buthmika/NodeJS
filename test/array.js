50d4df33b021571b51d790cfb71e766d4b4594fa
Buthmika <buthmikamenushan@gmail.com>
2025-04-14 08:57:19 +0530

    learn js basic

---
+++ test/array.js
@@ -0,0 +1,4 @@
+let name=["John", "Doe", "Smith"];
+console.log(name[0]);
+name[0]="Jane";
+console.log(name);
---
+++ test/circle.js
@@ -0,0 +1,4 @@
+let radious = 5;
+let pi = 3.14;
+let area = pi * radious * radious;
+console.log("Area of circle:"+ area);
