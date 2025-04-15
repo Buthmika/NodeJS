96b61cd2bb5f5164ac615f67e9ecc545bf7d7673
Buthmika <buthmikamenushan@gmail.com>
2025-04-15 08:14:37 +0530

    update

--- personManagmentConsoleApp/app.js
+++ personManagmentConsoleApp/app.js
@@ -1,7 +1,8 @@
-
+const chalk=require("chalk")
 const db = require("./db.js")
 
 require("./db.js")
 // console.log("app.js working")
 // console.log(db.add())
 // console.log(db.view())
+console.log(chalk.red("Hello World"))
